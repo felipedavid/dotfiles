0deee7c4d385ba571accbbc8cbb6ad1e5981aa8d
Felipe David <felipedavid.huh@gmail.com>
2021-02-27 13:17:32 -0300

    surf is too slow to use as main browser, unfortunately

--- surf/adblock_host.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone http://git.r-36.net/hosts-gen
-cd hosts-gen
-sudo make install
-sudo cp examples/gethostszero /bin
-sudo chmod 775 /bin/gethostszero
-sudo /bin/gethostszero
-sudo hosts-gen 
