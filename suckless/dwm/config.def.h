8b76aab64206f528f5a8c8b6db57c114df510987
Felipe David <felipedavid.huh@gmail.com>
2021-04-27 17:12:42 -0300

    .

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -1,25 +1,32 @@
+#include <X11/XF86keysym.h>
+
+// volume control
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 32;       /* snap piel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const int topbar             = 1;       /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=7.5" };
+static const char dmenufont[]       = "monospace:size=7.5";
+static const char col_gray1[]       = "#1D1F21";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#C5C8C6";
+static const char col_cyan[]        = "#282A2E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan},
+	[SchemeSel]  = { col_gray4, col_cyan,  "#c00404"},
 };
 
 /* tagging */
@@ -98,6 +105,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
+    { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
 };
 
 /* button definitions */
--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -15,9 +15,9 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=7" };
-static const char dmenufont[]       = "monospace:size=7";
+static const int topbar             = 1;       /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=7.5" };
+static const char dmenufont[]       = "monospace:size=7.5";
 static const char col_gray1[]       = "#1D1F21";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
