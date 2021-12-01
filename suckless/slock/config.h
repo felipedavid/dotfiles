03ba1bd8f7520ff365d8073a9c29b7cc38924842
Felipe David <felipedavid.huh@gmail.com>
2021-11-30 23:26:15 -0300

    .

--- suckless/slock/config.h
+++ suckless/slock/config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *user  = "smooth";
+static const char *group = "smooth";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
