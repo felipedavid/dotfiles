2d1924f73921f268a1412660dad7fd1f1d9aec66
Felipe David <felipedavid.huh@gmail.com>
2021-12-02 21:27:56 -0300

    allowing emojis on dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -20,8 +20,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=8" };
-static const char dmenufont[]       = "monospace:size=8";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=7.5" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=7.5";
 static const char col_gray1[]       = "#121212";
 static const char col_gray2[]       = "#232323";
 static const char col_gray3[]       = "#bbbbbb";
