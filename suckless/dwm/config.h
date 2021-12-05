1c87de0630dca555e54d151bf3f12d64d19d3118
Felipe David <felipedavid.huh@gmail.com>
2021-12-05 13:03:09 -0300

    Adding keybind to lock

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -77,13 +77,15 @@ static const char *dmenucmd[] = { "dmenu_run" , "-m", dmenumon, "-fn", dmenufont
 static const char *termcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 static const char *surfcmd[] = { "tabbed", "surf", "-e", NULL };
 static const char *zathuracmd[] = { "tabbed", "zathura", "-e", NULL };
+static const char *slockcmd[] = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = surfcmd } },
 	{ MODKEY,                       XK_z,      spawn,          {.v = zathuracmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
