8177747e75f07679328f48f4dd4249a6b5424d9d
Felipe David <felipedavid.huh@gmail.com>
2021-12-14 01:17:55 -0300

    .

--- suckless/tabbed/config.h
+++ suckless/tabbed/config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=8";
-static const char* normbgcolor  = "#232323";
+static const char font[]        = "JetBrainsMono Nerd Font:size=7.5";
+static const char* normbgcolor  = "#121212";
 static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
+static const char* selbgcolor   = "#232323";
 static const char* selfgcolor   = "#ffffff";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
@@ -33,16 +33,22 @@ static Bool npisrelative  = False;
         } \
 }
 
-#define MODKEY ControlMask
+#define MODKEY Mod1Mask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	//{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
+	//{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY,     XK_Return, focusonce,   { 0 } },
+	{ MODKEY,     XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	//{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
+	//{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY,               XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY,               XK_h,      rotate,      { .i = -1 } },
+	//{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
+	//{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_l,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_h,      movetab,     { .i = -1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
