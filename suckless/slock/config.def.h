b1154c7b29ef3ed4e7290267066b0822bfece58d
Felipe David <felipedavid.huh@gmail.com>
2021-12-15 20:24:07 -0300

    making slock looks noice

--- suckless/slock/config.def.h
+++ suckless/slock/config.def.h
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
--- suckless/slock/config.h
+++ suckless/slock/config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "smooth";
-static const char *group = "smooth";
+static const char *user  = "nobody";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
