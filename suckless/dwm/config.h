fede3d4a3bf6f93c34423b202d0ce86f9ed448dd
Felipe David <felipedavid.huh@gmail.com>
2022-02-14 18:37:42 -0300

    .

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=8" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=8";
+static const char *fonts[]          = { "ConsolasFont:size=8" };
+static const char dmenufont[]       = "Consolas:size=8";
 static const char col_gray0[]       = "#121212";
 static const char col_gray1[]       = "#232323";
 static const char col_gray2[]       = "#444444";
--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true";
+static char *font = "Consolas:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -120,7 +120,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"#f7f7f7", /* default foreground colour */
+	//"#f7f7f7", /* default foreground colour */
+	"#BBBBBB", /* default foreground colour */
 	"#000000", /* default background colour */
 };
 
