6ebc8c0c665591acc55e9a94b1fbf7f9e4980482
Felipe David <felipedavid.huh@gmail.com>
2022-03-07 05:31:17 -0300

    .

--- .scripts/timew.sh
+++ .scripts/timew.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-notify-send "$(/bin/timew $@)"
+if [[ $1 == "continue" || $1 == "stop" || $1 == "" ]]; then
+	notify-send "$(/bin/timew $@)"
+elif [[ $1 == "start" ]]; then
+	task_name=$(echo $(timew tags) | cut -c 70- | sed "s/ - /\n/g" | sed "s/ -$//g" | dmenu -i -l 20)
+	stat_msg=$(/bin/timew start "$task_name")
+	notify-send "$stat_msg"
+else
+	/bin/timew "$@"
+fi
