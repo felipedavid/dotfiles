d1c94410b6bafd8e32d973e9f1b5b304cc13b2b9
Felipe David <felipedavid.huh@gmail.com>
2022-09-10 09:35:21 -0300

    Making the terminal's tabbed session finish when the last terminal emulator exits

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark1, "-nf", col_gray4, "-sb", col_dark2, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
+static const char *termcmd[]  = { "tabbed", "-c", "-r", "2", "st", "-w", "''", NULL };
 
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
