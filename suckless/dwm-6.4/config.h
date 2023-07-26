a8d298ead01c19b539db421747f6dc7ce392cef5
Felipe David <felipedavid.huh@gmail.com>
2023-07-25 21:39:39 -0300

    Adding screenshot functionality with main

--- suckless/dwm-6.4/config.h
+++ suckless/dwm-6.4/config.h
@@ -62,11 +62,15 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *maim[] = { "maim", "~/screenshots/$(date +%F-%H_%M_%S).png", NULL };
+static const char *maim_select[] = { "maim", "-s", "~/screenshots/$(date +%F-%H_%M_%S).png", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = maim } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = maim_select } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
